071c57ae4c0b7a50b3d1bec2bfa281326de4255f
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-03-07 19:09:02 +0900

    Variableized numerical value

--- src/test.cpp
+++ src/test.cpp
@@ -4,47 +4,55 @@
 
 using namespace::std;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
   ros::init(argc, argv, "send_grid_cells");
 
   ros::NodeHandle nh;
 
   ros::Publisher pub = nh.advertise<nav_msgs::GridCells>("grid_cells", 100);
-  ros::Rate loop_rate(5);
+  // ros::Rate loop_rate(0.1);
+
+  float width = 19.2;
+  float length = 19.2;
+  float resolution = 0.06;
+  float resolution_Reciprocal = 1/resolution;
+  int loop_index = max(width, length) * resolution_Reciprocal;
+  int loop_width = width * resolution_Reciprocal;
+  int loop_length = length * resolution_Reciprocal;
 
   nav_msgs::GridCells msg;
-  int width = 19.2;
-  int length = 19.2;
-  msg.cells.resize(width * length*400);
+  msg.cells.resize(int(width * length* pow(1/resolution,2)));
   msg.header.frame_id = "map";
-  msg.cell_width = 0.05;
-  msg.cell_height = 0.05;
+  msg.cell_width = resolution;
+  msg.cell_height = resolution;
 
+  for (int x = 0; x < loop_width; x++)
+    for (int y = 0; y < loop_length; y++)
+      //Processing something....
+
+  ros::ok;  
+  ros::Duration duration(2);
+  duration.sleep();
+  
+  
   while (ros::ok())
   {
-    for (int x = 0; x < width*20; x++)
+    for (int x = 0; x < loop_width; ++x)
     {
-      // cout << "1" << "\n";
-      for (int y = 0; y < length*20; y++)
+      for (int y = 0; y < loop_length; ++y)
       {
-        // cout << "1" << "\n";
-        geometry_msgs::Point& point = msg.cells[x + ((y*20) * width)];
-        // cout << "2" << "\n";
-        // cout << x << " " << y << "\n";
-        point.x = float(x)/20;
-        point.y = float(y)/20;
-        // point.x = x;
-        // point.y = y;
+        geometry_msgs::Point& point = msg.cells[x + ( y*loop_index)];
+        point.x = float(x)*resolution;
+        point.y = float(y)*resolution;
         point.z = 0;
       }
+      msg.header.stamp = ros::Time::now();
+      pub.publish(msg);
+
+      ros::Duration duration(0.01);
+      duration.sleep();
     }
-
-    msg.header.stamp = ros::Time::now();
-
-    pub.publish(msg);
-
-    ros::spinOnce();
-    loop_rate.sleep();
+    // ros::spinOnce();
+    // loop_rate.sleep();
   }
 }
