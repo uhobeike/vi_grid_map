73ab8cb1f37c084e86c7aa075bfd05ab9f477aff
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-03-16 22:20:47 +0900

    Fix value

--- vi_grid_map/src/ViGridMap_node.cpp
+++ vi_grid_map/src/ViGridMap_node.cpp
@@ -7,8 +7,8 @@ using namespace::std;
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "vi_grid_map");
-    ros::NodeHandle nodeHandle;
-    vi_grid_map::ViGridMap vi(nodeHandle);
+    ros::NodeHandle nh;
+    vi_grid_map::ViGridMap vi(nh);
     
     ros::spin();
     return 0;
