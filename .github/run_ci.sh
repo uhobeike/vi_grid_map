456485dd92bfc4c876cde73a03dfbcdd10f5141e
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-05-12 21:28:26 +0900

    Fix ci

--- .github/run_ci.sh
+++ .github/run_ci.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd .. && cd ..
 
-docker pull ubeike/ros-ci_vi_grid_map
-
 docker run \
     -v $(pwd):/home/catkin_ws/src \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
@@ -15,4 +13,4 @@ ubeike/ros-ci_vi_grid_map /bin/bash -c \
     "git clone -b ikebe_dev https://github.com/ryuichiueda/value_iteration.git;
     rosdep install -r -y --from-paths --ignore-src .; 
     cd /home/catkin_ws && catkin build && source /home/catkin_ws/devel/setup.bash; 
-    xvfb-run --auto-servernum -screen 0 1400x900x24 roslaunch value_iteration vi_map.launch &"
+    xvfb-run --auto-servernum -screen 0 1400x900x24 roslaunch value_iteration vi_map.launch"
